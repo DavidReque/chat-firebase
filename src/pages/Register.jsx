95dc7228a7d45783afae1991d9ab56c4889b3667
David Requeno <davidrequeno52@gmail.com>
2023-04-09 19:49:14 -0600

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,24 @@
 const Register = () => {
-    return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">David Chat</span>
-                <span className="title">Register</span>
-                <form action="">
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input style={{display: 'none'}} type="file" id="file" />
-                    <label htmlFor="file"><img src="" alt="" /></label>
-                    <button>Sign ip</button>
-                </form>
-                <p>You do have an account? Login</p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">David Chat</span>
+        <span className="title">Register</span>
+        <form action="">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src="src/assets/img/addAvatar.png" alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign ip</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
