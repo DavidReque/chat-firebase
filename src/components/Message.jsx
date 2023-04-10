7f0b88868be4fc7b8fb66eedec235d460b8fb793
David Requeno <davidrequeno52@gmail.com>
2023-04-10 14:55:47 -0600

    Messages styles

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images" alt="" />
+      </div>
     </div>
   );
 }
