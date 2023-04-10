12926d679b8fbe691e9df3f720c5dd422e1c362d
David Requeno <davidrequeno52@gmail.com>
2023-04-10 14:15:46 -0600

    Chat navbar

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
   const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src="src/assets/img/cam.png" alt="" />
+          <img src="src/assets/img/add.png" alt="" />
+          <img src="src/assets/img/more.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
