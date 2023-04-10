28a2fc6634d53dd61054b18d525871277b169d80
David Requeno <davidrequeno52@gmail.com>
2023-04-10 14:08:59 -0600

    chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
-  const Chats = () => {
+const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
