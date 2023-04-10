480343900e5a100e2fbb5560cbd0a08f0b7ed778
David Requeno <davidrequeno52@gmail.com>
2023-04-09 21:11:14 -0600

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export const Search = () => {
+const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfq10Y4d1zOMcd_dvEw80AHaGR%26pid%3DApi&f=1&ipt=9cfdfffcc81b010e5f8fa7e80567a119d75dc2f467523dd1625eb7d271b1e47f&ipo=images"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar";
+import Search from "./Search";
 
 const SideBar = () => {
   return <div className="sidebar">
     <Navbar/>
-    
+    <Search/>
   </div>;
 };
 
