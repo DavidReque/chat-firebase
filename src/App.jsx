ff42272c8c2acd9d59629f164eeb0c62f10ae956
David Requeno <davidrequeno52@gmail.com>
2023-04-09 19:57:20 -0600

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import './styles.scss'
@@ -5,7 +6,7 @@ import './styles.scss'
 function App() {
 
   return (
-    <Login/>
+    <Home/>
   )
 }
 
