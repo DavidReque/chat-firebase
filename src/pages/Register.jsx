8b7e048876b5a6211931c712899dca7b2b59adb3
David Requeno <davidrequeno52@gmail.com>
2023-04-11 13:00:22 -0600

    auth

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,34 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+
 const Register = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">David Chat</span>
         <span className="title">Register</span>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
