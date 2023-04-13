903efce855fa43a402c13ef913f81cb53c2d5715
David Requeno <davidrequeno52@gmail.com>
2023-04-13 14:01:42 -0600

    style scroll

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react'
-import Messages from './Messages'
-import Input from './Input'
-import { ChatContext } from '../context/ChatContext'
+import React, { useContext } from "react";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
-  const Chat = () => {
-    const {data} = useContext(ChatContext)
+const Chat = () => {
+  const { data } = useContext(ChatContext);
 
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chatInfo">
-        <span>{data.user?.displayName}</span>
+        <span style={{ fontWeight: "bold" }}>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src="src/assets/img/cam.png" alt="" />
           <img src="src/assets/img/add.png" alt="" />
           <img src="src/assets/img/more.png" alt="" />
         </div>
       </div>
-      <Messages/>
-      <Input/>
+      <Messages />
+      <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
